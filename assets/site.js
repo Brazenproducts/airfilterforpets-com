1e183bd8f599861ebfee634f29b71529f19a296c
Brazenproducts <info@brazenauto.com>
2026-05-16 18:17:43 +0000

    Add privacy policy and missing pages

---
+++ assets/site.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('details').forEach((item) => {
+  item.addEventListener('toggle', () => {
+    if (!item.open) {
+      return;
+    }
+
+    document.querySelectorAll('details').forEach((other) => {
+      if (other !== item) {
+        other.open = false;
+      }
+    });
+  });
+});
